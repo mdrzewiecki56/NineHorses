03bc75265c0708361eaf406db017d621b2ba177c
swidziniak <lukasz.swidz@gmail.com>
2020-04-26 13:47:14 +0200

    add border to the center field, change favicon, add board status variables

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,48 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Field from '../Field/Field';
-import {chunkArray} from '../../Helper';
+import { chunkArray } from '../../Helper';
 import './Board.scss';
 
+var CUR_PLAYER_W = 0;
+var CUR_PLAYER_B = 1;
+
 function Board(props) {
-    //VALIDATE BOARD SIZE
-    let size;
-    if (props.size === undefined || props.size < 5 || props.size %2!==1) {
-        console.log("Invalid board size!!")
-        size = 9
-    } else {
-        size = props.size;
+  // STATUS VARIABLES
+  // White by default as white always starts
+  const [currentPlayer, setCurrentPlayer] = useState(CUR_PLAYER_W);
+  // Field from which player wants to make a move
+  const [selectedField, setSelectedField] = useState(null);
+
+  //VALIDATE BOARD SIZE
+  let size;
+  // Board size must be greater than 5 and odd-sized
+  if (props.size === undefined || props.size < 5 || props.size % 2 !== 1) {
+    console.log('Invalid board size!!');
+    size = 9;
+  } else {
+    size = props.size;
+  }
+
+  //BOARD CREATION
+  let dimension = size * size;
+
+  let createBoard = (dimension) => {
+    let i, j;
+    for (let k = 0; k < dimension; k++) {
+      // Calculate Indexes
+      i = Math.ceil((k + 1) / size);
+      j = (k + 1) % size;
+      if (j === 0) j = size;
+    }
+    let fields = [];
+    for (let l = 0; l < i; l++) {
+      for (let m = 0; m < j; m++) {
+        fields.push(<Field row={l} column={m} key={`${l}:${m}`} />);
+      }
     }
 
-    //BOARD CREATION
-    let dimension = size*size;
-    
-    let createBoard = (dimension) => {
-        let i,j;
-        for (let k = 0; k < dimension; k++) {
-            // Calculate Indexes
-            i = Math.ceil((k + 1) / size);
-            j = (k + 1) % size;
-            if (j === 0) j = size;
-        }
-        let fields = [];
-        for (let l=0;l<i;l++){
-            for (let m=0;m<j;m++) {
-                fields.push(<Field row={l} column={m} key={`${l}:${m}`}/>)
-            }
-        }
+    return chunkArray(fields, size);
+  };
 
-        return chunkArray(fields,size);
-    }
+  let board = createBoard(dimension).map((columns, index) => (
+    <div className='row' key={`column${index}`}>
+      {columns}
+    </div>
+  ));
 
-    let board = createBoard(dimension).map((columns,index) => 
-        <div className="row" key={`column${index}`}>{columns}</div>);
-
-    //RENDERER
-    return (
-        <div className="Board">
-            {board}
-        </div>
-    );
+  //RENDERER
+  return <div className='Board'>{board}</div>;
 }
 
 export default Board;
--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function Field(props){
-    return (
-        <div className="Field">
-            {props.row}:{props.column}
-        </div>
-    );
+export default function Field(props) {
+  return (
+    <div
+      className={
+        'Field ' + (props.row === 4 && props.column === 4 ? 'CenterField' : '')
+      }
+    >
+      {props.row}:{props.column}
+    </div>
+  );
 }
