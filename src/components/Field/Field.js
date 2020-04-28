94c3b8cdeb614574b8692b1b059f95977770a6bf
unknown <mdrzewiecki56@gmail.com>
2020-04-28 18:40:22 +0200

    Add horse figures

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Horse from '../Horse';
 import './Field.scss';
+import wHorse from './wHorse.svg';
+import bHorse from './bHorse.svg'
+
 
 export default function Field(props) {
   return (
     <div
       className={`Field ${props.isCenter ? 'center' : ''}`}
     >
-      {props.Horse && <div className={props.classList}>Horse</div>}
+      {
+        props.Horse && 
+        <div className={props.classList}>
+          {props.Horse.mode===Horse.HORSE_W && <img src={wHorse} alt="white-horse" className="pawn"/>}
+          {props.Horse.mode===Horse.HORSE_B && <img src={bHorse} alt="black-horse" className="pawn"/>}
+        </div>
+      }
     </div>
   );
 }
