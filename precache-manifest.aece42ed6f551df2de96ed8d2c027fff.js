2ae6b059e2d50793223e80f34d8501da30a1dc52
Maciek <mdrzewiecki56>
2020-06-16 16:17:24 +0200

    Updates

---
+++ precache-manifest.aece42ed6f551df2de96ed8d2c027fff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec205c29b232ef075780a197e76fc49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77ce471edae4d3cbbd9a",
+    "url": "/static/css/main.1a731d07.chunk.css"
+  },
+  {
+    "revision": "ede577077a4ce235767e",
+    "url": "/static/js/2.fa579ed6.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.fa579ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ce471edae4d3cbbd9a",
+    "url": "/static/js/main.1262029d.chunk.js"
+  },
+  {
+    "revision": "82486c8f1361d2cc04cb",
+    "url": "/static/js/runtime-main.4cbb7936.js"
+  },
+  {
+    "revision": "3e68d80027478f5f5ef45fa7face842c",
+    "url": "/static/media/black.3e68d800.svg"
+  },
+  {
+    "revision": "83c6f6841e057bc82c753151f9357bb2",
+    "url": "/static/media/white.83c6f684.svg"
+  }
+]);
